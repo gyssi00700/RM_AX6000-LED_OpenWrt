ab08060c84e0cb44e65a41043f03fec31a8e11b6
gyssi007 <78358246+gyssi007@users.noreply.github.com>
2024-08-04 18:50:28 +0800

    Update diy-part1.sh

--- padavanonly/Redmi-AX6000/diy-part1.sh
+++ padavanonly/Redmi-AX6000/diy-part1.sh
@@ -16,4 +16,6 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git messense https://github.com/messense/aliyundrive-webdav' >>feeds.conf.default
+echo 'src-git sirpdboy https://github.com/sirpdboy/luci-app-ddns-go.git
+echo 'src-git alistorg https://github.com/alist-org/alist.git
+echo 'src-git ximiTech https://github.com/ximiTech/luci-app-msd_lite.git
